5ff48b5728aaac721a896c442c675a2eaa9cedc0
IngDanielEN <danieles1217@gmail.com>
2025-05-04 20:39:54 -0500

    routes correction

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -4,15 +4,15 @@ import { searchProducts, getProductById, createProduct, updateProduct } from '..
 const router = Router();
 
 // Search products
-router.get('/search', searchProducts as RequestHandler);
+router.get('/items', searchProducts as RequestHandler);
 
 // Get product by ID
-router.get('/:id', getProductById as RequestHandler);
+router.get('/items/:id', getProductById as RequestHandler);
 
 // Create product
-router.post('/', createProduct as RequestHandler);
+router.post('/items', createProduct as RequestHandler);
 
 // Update product
-router.put('/:id', updateProduct as RequestHandler);
+router.put('/items/:id', updateProduct as RequestHandler);
 
 export default router; 
